b7e7669b2646d4f28faba169e473bf30531651a0
BozhenaVlasova <dix20sept@gmail.com>
2022-11-13 20:31:30 +0300

    manipulating complex objects

---
+++ freecodecamp/js/basic-js/manipulating-complex-objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Lady Gaga",
+    "title": "Bad Romance",
+    "release_year": 2009,
+    "formats": [
+      "CD",
+      "LP"
+    ]
+  }
+];
