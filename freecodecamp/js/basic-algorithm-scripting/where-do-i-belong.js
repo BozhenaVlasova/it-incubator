20f19a04e3572c60390e921f9c98e731345c528c
BozhenaVlasova <dix20sept@gmail.com>
2022-11-22 16:06:03 +0300

    where do i belong

---
+++ freecodecamp/js/basic-algorithm-scripting/where-do-i-belong.js
@@ -0,0 +1,10 @@
+function getIndexToIns(arr, num) {
+  let sorted = arr.sort((a, b) => a - b);
+  for (let i = 0; i < sorted.length; i++) {
+    if (num > arr[i-1] && num <= arr[i]) return i;
+  }
+  if (sorted[sorted.length - 1] < num) return sorted.length;
+  return 0;
+}
+
+getIndexToIns([40, 60], 50);
