bd4245d1dae1afa9f51caf60ac796723f33243f2
BozhenaVlasova <dix20sept@gmail.com>
2022-11-24 12:24:33 +0300

    split string into an array using the split method

---
+++ freecodecamp/js/functional-programming/split-a-string-into-an-array-using-the-split-method.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Only change code below this line
+  let res = str.split(/[ ,-.]/i);
+  return res;
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
