08f06959f11ba14bab95f38f9434123e23238249
BozhenaVlasova <dix20sept@gmail.com>
2022-11-30 21:56:40 +0300

    Find the capitals

---
+++ codewars-js/capitals.js
@@ -0,0 +1,9 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list 
+// containing the indexes of all capital letters in the string.
+
+var capitals = function (word) {
+    let arr = word.split("");
+    return  arr.reduce((accum, item, index) => { 
+        if (item === item.toUpperCase()) accum.push(index); return accum}, [])
+};
